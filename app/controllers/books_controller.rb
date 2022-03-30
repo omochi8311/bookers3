024fe1ac961d1919b7b70d6a94e021c73bbf1219
EC2 Default User <ec2-user@ip-172-31-28-130.ec2.internal>
2022-03-30 13:42:20 +0000

    投稿一覧まで完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,29 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+
+  book = Book.new(books_params)
+  book.save
+  redirect_to’/show’
+  end
+
   def index
+
+    @book = Book.all
+
   end
 
   def show
+
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).parmit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'books/new'
-  get 'books/index'
+  get 'books/' => 'books#index'
+  post 'books' => 'books#create'
+  put 'books' => 'book#index#'
   get 'books/show'
   get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
