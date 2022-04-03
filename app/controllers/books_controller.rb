768ec52f035668ff42e061318547e6f0be7b0e7b
EC2 Default User <ec2-user@ip-172-31-28-130.ec2.internal>
2022-04-03 09:09:15 +0000

    showへのリンクエラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
   book = Book.new(book_params)
@@ -11,15 +8,16 @@ class BooksController < ApplicationController
 
   def index
 
-    @book = Book.all
-
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book =Book.find(params[:id])
   end
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'books/' => 'books#index'
   post 'books' => 'books#create'
   put 'books' => 'book#index#'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
